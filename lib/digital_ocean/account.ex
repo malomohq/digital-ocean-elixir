39fd3f44f0c969cb6f2390917716a743d6565e63
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-09-19 15:30:27 -0400

    Support all account endpoints

---
+++ lib/digital_ocean/account.ex
@@ -0,0 +1,18 @@
+defmodule DigitalOcean.Account do
+  alias DigitalOcean.{ Operation }
+
+  @doc """
+  Show information for the current user's account.
+
+  ## Examples
+
+      iex> DigitalOcean.Account.get() |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec get :: Operation.t()
+  def get do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:path, "/account")
+  end
+end
---
+++ test/digital_ocean/account_test.exs
@@ -0,0 +1,13 @@
+defmodule DigitalOcean.AccountTest do
+  use ExUnit.Case, async: true
+
+  alias DigitalOcean.{ Account, Operation }
+
+  test "get/0" do
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :get)
+    expected = Map.put(expected, :path, "/account")
+
+    assert expected == Account.get()
+  end
+end
