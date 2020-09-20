90265f8e1bdf4770a4addb126a1527f6e63b8cad
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-09-20 16:29:02 -0400

    Support all CDN endpoints

---
+++ lib/digital_ocean/cdn_endpoint.ex
@@ -0,0 +1,101 @@
+defmodule DigitalOcean.CDNEndpoint do
+  alias DigitalOcean.{ Operation }
+
+  @doc """
+  Create a CDN endpoint.
+
+  ## Example
+
+      iex> DigitalOcean.CDNEndpoint.create("static-images.nyc3.digitaloceanspaces.com") |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec create(String.t(), Keyword.t()) :: Operation.t()
+  def create(origin, opts \\ []) do
+    opts = Keyword.put(opts, :origin, origin)
+
+    %Operation{}
+    |> Map.put(:method, :post)
+    |> Map.put(:params, opts)
+    |> Map.put(:path, "/cdn/endpoints")
+  end
+
+  @doc """
+  Delete a CDN endpoint.
+
+  ## Example
+
+      iex> DigitalOcean.CDNEndpoint.delete("19f06b6a-3ace-4315-b086-499a0e521b76") |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec delete(String.t()) :: Operation.t()
+  def delete(endpoint_id) do
+    %Operation{}
+    |> Map.put(:method, :delete)
+    |> Map.put(:path, "/cdn/endpoints/#{endpoint_id}")
+  end
+
+  @doc """
+  Retrieve details about a specific CDN endpoint.
+
+  ## Example
+
+      iex> DigitalOcean.CDNEndpoint.get("19f06b6a-3ace-4315-b086-499a0e521b76") |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec get(String.t()) :: Operation.t()
+  def get(endpoint_id) do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:path, "/cdn/endpoints/#{endpoint_id}")
+  end
+
+  @doc """
+  Retrieve a list of all CDN endpoints.
+
+  ## Examples
+
+      iex> DigitalOcean.CDNEndpoint.list() |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec list(Keyword.t()) :: Operation.t()
+  def list(opts \\ []) do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:params, opts)
+    |> Map.put(:path, "/cdn/endpoints")
+  end
+
+  @doc """
+  Purge cached content from an CDN endpoint.
+
+  ## Examples
+
+      iex> DigitalOcean.CDNEndpoint.purge_cache("19f06b6a-3ace-4315-b086-499a0e521b76", ["*"])
+      { :ok, %DigitalOcean.Response{} }
+  """
+  def purge_cache(endpoint_id, files) do
+    params = Keyword.new()
+    params = Keyword.put(params, :files, files)
+
+    %Operation{}
+    |> Map.put(:method, :delete)
+    |> Map.put(:params, params)
+    |> Map.put(:path, "/cdn/endpoints/#{endpoint_id}/cache")
+  end
+
+  @doc """
+  Update a CDN endpoint.
+
+  ## Example
+
+      iex: DigitalOcean.CDNEndpoint.update("19f06b6a-3ace-4315-b086-499a0e521b76", ttl: 1800)
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec update(String.t(), Keyword.t()) :: Operation.t()
+  def update(endpoint_id, opts \\ []) do
+    %Operation{}
+    |> Map.put(:method, :put)
+    |> Map.put(:params, opts)
+    |> Map.put(:path, "/cdn/endpoints/#{endpoint_id}")
+  end
+end
---
+++ test/digital_ocean/cdn_endpoint_test.exs
@@ -0,0 +1,75 @@
+defmodule DigitalOcean.CDNEndpointTest do
+  use ExUnit.Case, async: true
+
+  alias DigitalOcean.{ CDNEndpoint, Operation }
+
+  test "create/2" do
+    origin = "static-images.nyc3.digitaloceanspaces.com"
+
+    ttl = 3600
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :post)
+    expected = Map.put(expected, :params, [origin: origin, ttl: ttl])
+    expected = Map.put(expected, :path, "/cdn/endpoints")
+
+    assert expected == CDNEndpoint.create(origin, ttl: ttl)
+  end
+
+  test "delete/1" do
+    endpoint_id = "19f06b6a-3ace-4315-b086-499a0e521b76"
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :delete)
+    expected = Map.put(expected, :path, "/cdn/endpoints/#{endpoint_id}")
+
+    assert expected == CDNEndpoint.delete(endpoint_id)
+  end
+
+  test "get/1" do
+    endpoint_id = "19f06b6a-3ace-4315-b086-499a0e521b76"
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :get)
+    expected = Map.put(expected, :path, "/cdn/endpoints/#{endpoint_id}")
+
+    assert expected == CDNEndpoint.get(endpoint_id)
+  end
+
+  test "list/1" do
+    page = 1
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :get)
+    expected = Map.put(expected, :params, [page: page])
+    expected = Map.put(expected, :path, "/cdn/endpoints")
+
+    assert expected == CDNEndpoint.list(page: page)
+  end
+
+  test "purge_cache/2" do
+    endpoint_id = "19f06b6a-3ace-4315-b086-499a0e521b76"
+
+    files = ["*"]
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :delete)
+    expected = Map.put(expected, :params, [files: files])
+    expected = Map.put(expected, :path, "/cdn/endpoints/#{endpoint_id}/cache")
+
+    assert expected == CDNEndpoint.purge_cache(endpoint_id, files)
+  end
+
+  test "update/2" do
+    endpoint_id = "19f06b6a-3ace-4315-b086-499a0e521b76"
+
+    ttl = 1800
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :put)
+    expected = Map.put(expected, :params, [ttl: ttl])
+    expected = Map.put(expected, :path, "/cdn/endpoints/#{endpoint_id}")
+
+    assert expected == CDNEndpoint.update(endpoint_id, ttl: ttl)
+  end
+end
