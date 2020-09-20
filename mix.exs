7165522f74a5238162e587ef0b89a9f7f36cc120
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-09-20 13:14:19 -0400

    Support elixir 1.8+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DigitalOcean.MixProject do
     [
       app: :digital_ocean,
       version: "0.0.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
