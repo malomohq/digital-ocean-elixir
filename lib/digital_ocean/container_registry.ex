77f98b3d0dab3d64113569c94062ec902759aaaf
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-09-20 19:48:06 -0400

    Support all container registry endpoints

---
+++ lib/digital_ocean/container_registry.ex
@@ -0,0 +1,142 @@
+defmodule DigitalOcean.ContainerRegistry do
+  alias DigitalOcean.{ Operation }
+
+  @doc """
+  Configure your container registry.
+
+  ## Examples
+
+      iex> DigitalOcean.ContainerRegistry.configure("example") |> DigitalOcean.request()
+      { :ok, DigitalOcean.Response{} }
+  """
+  @spec configure(String.t()) :: Operation.t()
+  def configure(container_registry_name) do
+    %Operation{}
+    |> Map.put(:method, :post)
+    |> Map.put(:params, [name: container_registry_name])
+    |> Map.put(:path, "/registry")
+  end
+
+  @doc """
+  Delete your container registry.
+
+  ## Examples
+
+      iex> DigitalOcean.ContainerRegistry.delete() |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec delete :: Operation.t()
+  def delete do
+    %Operation{}
+    |> Map.put(:method, :delete)
+    |> Map.put(:path, "/registry")
+  end
+
+  @doc """
+  Delete a container registry repository by digest.
+
+  ## Examples
+
+      iex> DigitalOcean.ContainerRegistry.delete_repository_manifest_by_digest("example", "repo-1", "sha256:cb8a924afdf0229ef7515d9e5b3024e23b3eb03ddbba287f4a19c6ac90b8d221") |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec delete_repository_manifest_by_digest(String.t(), String.t(), String.t()) :: Operation.t()
+  def delete_repository_manifest_by_digest(container_registry_name, repository_name, digest) do
+    %Operation{}
+    |> Map.put(:method, :delete)
+    |> Map.put(:path, "/registry/#{container_registry_name}/repositories/#{repository_name}/digests/#{digest}")
+  end
+
+  @doc """
+  Delete a repository tag in your container registry.
+
+  ## Examples
+
+      iex> DigitalOcean.ContainerRegistry.delete_repository_tag("example", "repo-1", "mytag") |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec delete_repository_tag(String.t(), String.t(), String.t()) :: Operation.t()
+  def delete_repository_tag(container_registry_name, repository_name, tag) do
+    %Operation{}
+    |> Map.put(:method, :delete)
+    |> Map.put(:path, "/registry/#{container_registry_name}/repositories/#{repository_name}/tags/#{tag}")
+  end
+
+  @doc """
+  Retrieve information about your container registry.
+
+  ## Examples
+
+      iex> DigitalOcean.ContainerRegistry.get() |> DigitalOcean.request()
+      { :ok, DigitalOcean.Response{} }
+  """
+  @spec get :: Operation.t()
+  def get do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:path, "/registry")
+  end
+
+  @doc """
+  Retrieve Docker credentials for your container registry.
+
+  ## Examples
+
+      iex> DigitalOcean.ContainerRegistry.get_docker_credentials() |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec get_docker_credentials :: Operation.t()
+  def get_docker_credentials do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:path, "/registry/docker-credentials")
+  end
+
+  @doc """
+  Retrieve a list of repositories in your container registry.
+
+  ## Examples
+
+      iex> DigitalOcean.ContainerRegistry.list_repositories("example") |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec list_repositories(String.t(), Keyword.t()) :: Operation.t()
+  def list_repositories(container_registry_name, opts \\ []) do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:params, opts)
+    |> Map.put(:path, "/registry/#{container_registry_name}/repositories")
+  end
+
+  @doc """
+  Retrieve a list of in a repository.
+
+  ## Examples
+
+      iex> DigitalOcean.ContainerRegistry.list_repository_tags("example", "repo-1") |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec list_repository_tags(String.t(), String.t()) :: Operation.t()
+  def list_repository_tags(container_registry_name, repository_name, opts \\ []) do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:params, opts)
+    |> Map.put(:path, "/registry/#{container_registry_name}/repositories/#{repository_name}/tags")
+  end
+
+  @doc """
+  Validate a container registry name.
+
+  ## Examples
+
+      iex> DigitalOcean.ContainerRegistry.validate_name("example") |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec validate_name(String.t()) :: Operation.t()
+  def validate_name(container_registry_name) do
+    %Operation{}
+    |> Map.put(:method, :post)
+    |> Map.put(:params, [name: container_registry_name])
+    |> Map.put(:path, "/registry/validate-name")
+  end
+end
---
+++ test/digital_ocean/container_registry_test.exs
@@ -0,0 +1,107 @@
+defmodule DigitalOcean.ContainerRegistryTest do
+  use ExUnit.Case, async: true
+
+  alias DigitalOcean.{ ContainerRegistry, Operation }
+
+  test "configure/1" do
+    name = "example"
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :post)
+    expected = Map.put(expected, :params, [name: name])
+    expected = Map.put(expected, :path, "/registry")
+
+    assert expected == ContainerRegistry.configure(name)
+  end
+
+  test "delete/0" do
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :delete)
+    expected = Map.put(expected, :path, "/registry")
+
+    assert expected == ContainerRegistry.delete()
+  end
+
+  test "delete_repository_manifest_by_digest/3" do
+    container_registry_name = "example"
+
+    repository_name = "repo-1"
+
+    digest = "sha256:cb8a924afdf0229ef7515d9e5b3024e23b3eb03ddbba287f4a19c6ac90b8d221"
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :delete)
+    expected = Map.put(expected, :path, "/registry/#{container_registry_name}/repositories/#{repository_name}/digests/#{digest}")
+
+    assert expected == ContainerRegistry.delete_repository_manifest_by_digest(container_registry_name, repository_name, digest)
+  end
+
+  test "delete_repository_tag/3" do
+    container_registry_name = "example"
+
+    repository_name = "repo-1"
+
+    tag = "mytag"
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :delete)
+    expected = Map.put(expected, :path, "/registry/#{container_registry_name}/repositories/#{repository_name}/tags/#{tag}")
+
+    assert expected == ContainerRegistry.delete_repository_tag(container_registry_name, repository_name, tag)
+  end
+
+  test "get/0" do
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :get)
+    expected = Map.put(expected, :path, "/registry")
+
+    assert expected == ContainerRegistry.get()
+  end
+
+  test "get_docker_credentials/0" do
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :get)
+    expected = Map.put(expected, :path, "/registry/docker-credentials")
+
+    assert expected == ContainerRegistry.get_docker_credentials()
+  end
+
+  test "list_repositories/1" do
+    name = "example"
+
+    page = 1
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :get)
+    expected = Map.put(expected, :params, [page: page])
+    expected = Map.put(expected, :path, "/registry/#{name}/repositories")
+
+    assert expected == ContainerRegistry.list_repositories(name, page: page)
+  end
+
+  test "list_repository_tags/3" do
+    container_registry_name = "exmple"
+
+    repository_name = "repo-1"
+
+    page = 1
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :get)
+    expected = Map.put(expected, :params, [page: page])
+    expected = Map.put(expected, :path, "/registry/#{container_registry_name}/repositories/#{repository_name}/tags")
+
+    assert expected == ContainerRegistry.list_repository_tags(container_registry_name, repository_name, page: page)
+  end
+
+  test "validate_name/1" do
+    name = "example"
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :post)
+    expected = Map.put(expected, :params, [name: name])
+    expected = Map.put(expected, :path, "/registry/validate-name")
+
+    assert expected == ContainerRegistry.validate_name(name)
+  end
+end
