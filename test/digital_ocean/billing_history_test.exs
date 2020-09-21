3a1b4b104e5ae4bfe153e6ed8d6b277e3205fdfc
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-09-21 08:56:08 -0400

    Test passing params to BillingHistory.list/1

--- test/digital_ocean/billing_history_test.exs
+++ test/digital_ocean/billing_history_test.exs
@@ -4,10 +4,13 @@ defmodule DigitalOcean.BillingHistoryTest do
   alias DigitalOcean.{ BillingHistory, Operation }
 
   test "list/1" do
+    page = 1
+
     expected = %Operation{}
     expected = Map.put(expected, :method, :get)
+    expected = Map.put(expected, :params, [page: page])
     expected = Map.put(expected, :path, "/customers/my/billing_history")
 
-    assert expected == BillingHistory.list()
+    assert expected == BillingHistory.list(page: page)
   end
 end
