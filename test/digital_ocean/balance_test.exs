475bc42ba92aef0fe6d4ff67b3b6b760a2074753
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-09-21 08:54:11 -0400

    Clean up

--- test/digital_ocean/balance_test.exs
+++ test/digital_ocean/balance_test.exs
@@ -3,7 +3,7 @@ defmodule DigitalOcean.BalanceTest do
 
   alias DigitalOcean.{ Balance, Operation }
 
-  test "get/1" do
+  test "get/0" do
     expected = %Operation{}
     expected = Map.put(expected, :method, :get)
     expected = Map.put(expected, :path, "/customers/my/balance")
