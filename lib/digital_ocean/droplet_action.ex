af34cb1682c7a4114d103cb3c1f31ad3e515d33e
Anthony Smith <420061+anthonator@users.noreply.github.com>
2020-09-21 18:10:44 -0400

    Support all droplet action endpoints

---
+++ lib/digital_ocean/droplet_action.ex
@@ -0,0 +1,55 @@
+defmodule DigitalOcean.DropletAction do
+  alias DigitalOcean.{ Operation }
+
+  @doc """
+  Create an action for a droplet.
+
+  ## Example enabling backups for a droplet
+
+      iex> DigitalOcean.DropletAction.create(
+      ...>   3164450,
+      ...>   type: "enable_backups"
+      ...> ) |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+
+  See the official DigitalOcean v2 API documentation for all options regariding
+  creating a droplet action.
+  """
+  @spec create(integer, Keyword.t()) :: Operation.t()
+  def create(droplet_id, opts) do
+    %Operation{}
+    |> Map.put(:method, :post)
+    |> Map.put(:params, opts)
+    |> Map.put(:path, "/droplets/#{droplet_id}/actions")
+  end
+
+  @doc """
+  Create an action for droplets by a tag.
+
+  ## Examples
+
+      iex> DigitalOcean.DropletAction.create_by_tag_name("awesome") |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec create_by_tag_name(String.t()) :: Operation.t()
+  def create_by_tag_name(tag_name) do
+    %Operation{}
+    |> Map.put(:method, :post)
+    |> Map.put(:path, "/droplets/actions?tag_name=#{tag_name}")
+  end
+
+  @doc """
+  Retrieve an action for a droplet.
+
+  ## Examples
+
+      iex> DigitalOcean.DropletAction.get(3164444, 36804807) |> DigitalOcean.request()
+      { :ok, %DigitalOcean.Response{} }
+  """
+  @spec get(integer, integer) :: Operation.t()
+  def get(droplet_id, action_id) do
+    %Operation{}
+    |> Map.put(:method, :get)
+    |> Map.put(:path, "/droplets/#{droplet_id}/actions/#{action_id}")
+  end
+end
---
+++ test/digital_ocean/droplet_action_test.exs
@@ -0,0 +1,40 @@
+defmodule DigitalOcean.DropletActionTest do
+  use ExUnit.Case, async: true
+
+  alias DigitalOcean.{ DropletAction, Operation }
+
+  test "create/2" do
+    droplet_id = 3164450
+
+    type = "enable_backups"
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :post)
+    expected = Map.put(expected, :params, [type: type])
+    expected = Map.put(expected, :path, "/droplets/#{droplet_id}/actions")
+
+    assert expected == DropletAction.create(droplet_id, type: type)
+  end
+
+  test "create_by_tag_name" do
+    tag_name = "awesome"
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :post)
+    expected = Map.put(expected, :path, "/droplets/actions?tag_name=#{tag_name}")
+
+    assert expected == DropletAction.create_by_tag_name(tag_name)
+  end
+
+  test "get/2" do
+    droplet_id = 3164444
+
+    action_id = 3164444
+
+    expected = %Operation{}
+    expected = Map.put(expected, :method, :get)
+    expected = Map.put(expected, :path, "/droplets/#{droplet_id}/actions/#{action_id}")
+
+    assert expected == DropletAction.get(droplet_id, action_id)
+  end
+end
